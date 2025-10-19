cbf216932d3ac882a2937d99284b50bb3af69b5e
Alex <alex.tashiguano@educa.madrid.org>
2025-10-19 10:25:47 -0500

    v3LS

--- src/main/java/es/etg/dam/Main.java
+++ src/main/java/es/etg/dam/Main.java
@@ -1,5 +1,10 @@
 package es.etg.dam;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
 public class Main {
 
     private static final String [] COMANDO = {"ls"};
@@ -12,6 +17,24 @@ public class Main {
     }
     
     public static String execComando (String [] comm, String input)throws Exception{
+        Process process = Runtime.getRuntime().exec(COMANDO);
+
+        if(input!=null){
+            try(PrintWriter pw = new PrintWriter (new OutputStreamWriter((process.getOutputStream())))){
+                pw.print(input);
+                pw.flush();
+            } catch (Exception e) {
+            }
+        }
+        StringBuilder builder = new StringBuilder();
+        try(BufferedReader bf = new BufferedReader(new InputStreamReader(process.getInputStream()))){
+            String line;
+            While((line=bf.readLine())!=null) {
+                builder.append(line).append("/n");
+
+            }
+        }
+        
 
     }
     
