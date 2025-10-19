cedf4cc5c7e52d8584e820ade937f7fae1be9d0e
Alex <alex.tashiguano@educa.madrid.org>
2025-10-19 10:48:31 -0500

    v4LS

--- src/main/java/es/etg/dam/Main.java
+++ src/main/java/es/etg/dam/Main.java
@@ -7,39 +7,42 @@ import java.io.PrintWriter;
 
 public class Main {
 
-    private static final String [] COMANDO = {"ls"};
-    private static final String [] COMANDO1 = {"grep"};
-    public static void main(String[] args) throws Exception{
+    private static final String[] COMANDO = {"ls"};
+    private static final String[] COMANDO1 = {"grep","-i", "a"};
+    private static final String MENSAJE = "Error al ejecutar el comando";
+
+    public static void main(String[] args) throws Exception {
 
         String ejecutar = execComando(COMANDO, null);
-        String ejecutar1 = execComando(COMANDO1);
+        String ejecutar1 = execComando(COMANDO1, ejecutar);
+        System.out.println(ejecutar1);
 
     }
-    
-    public static String execComando (String [] comm, String input)throws Exception{
-        Process process = Runtime.getRuntime().exec(COMANDO);
 
-        if(input!=null){
-            try(PrintWriter pw = new PrintWriter (new OutputStreamWriter((process.getOutputStream())))){
+    public static String execComando(String[] comm, String input) throws Exception {
+        Process process = Runtime.getRuntime().exec(comm);
+
+        if (input != null) {
+            try (PrintWriter pw = new PrintWriter(new OutputStreamWriter((process.getOutputStream())))) {
                 pw.print(input);
                 pw.flush();
             } catch (Exception e) {
             }
         }
         StringBuilder builder = new StringBuilder();
-        try(BufferedReader bf = new BufferedReader(new InputStreamReader(process.getInputStream()))){
+        try (BufferedReader bf = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
             String line;
-            While((line=bf.readLine())!=null) {
-                builder.append(line).append("/n");
+            while ((line = bf.readLine()) != null) {
+                builder.append(line).append("\n");
 
             }
         }
-        
 
+        int salida = process.waitFor();
+        if (salida == 0) {
+            return builder.toString();
+        } else {
+            return MENSAJE + String.join(" ", comm);
+        }
     }
-    
-
-
-
-
-    }
+}
