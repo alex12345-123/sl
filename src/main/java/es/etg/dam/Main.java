0f86d838f9bfdb8f0d6c3a9f6f116720f485ee00
Alex <alex.tashiguano@educa.madrid.org>
2025-10-19 10:12:42 -0500

    v2LS

--- src/main/java/es/etg/dam/Main.java
+++ src/main/java/es/etg/dam/Main.java
@@ -1,7 +1,22 @@
 package es.etg.dam;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    private static final String [] COMANDO = {"ls"};
+    private static final String [] COMANDO1 = {"grep"};
+    public static void main(String[] args) throws Exception{
+
+        String ejecutar = execComando(COMANDO, null);
+        String ejecutar1 = execComando(COMANDO1);
+
     }
-}
+    
+    public static String execComando (String [] comm, String input)throws Exception{
+
+    }
+    
+
+
+
+
+    }
